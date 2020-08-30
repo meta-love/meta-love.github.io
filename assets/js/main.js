9f3d0f99bb2be95271304df4aef2c265cf74215c
taosue <taosue314@gmail.com>
2020-08-30 11:54:33 +0800

    fix some bug

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,7 +8,7 @@
   "use strict";
 
   // Preloader
-  $(window).on('DOMContentLoaded', function() {
+  $(window).on('load', function() {
     if ($('#preloader').length) {
       $('#preloader').delay(100).fadeOut('slow', function() {
         $(this).remove();
